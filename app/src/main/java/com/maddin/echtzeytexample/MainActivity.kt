60d93d13458dfd1f473aea52badfa0754a570bd3
Martin <martin@martinkunze.com>
2023-08-09 16:21:21 +0200

    added widget functionality

--- app/src/main/java/com/maddin/echtzeytexample/MainActivity.kt
+++ app/src/main/java/com/maddin/echtzeytexample/MainActivity.kt
@@ -1,9 +1,17 @@
-package com.maddin.echtzeytexample // <CUSTOMIZE>
+package com.maddin.echtzeytexample
+
+import com.maddin.echtzeyt.EchtzeytWidget
+
+// <CUSTOMIZE>
+
+val TRANSPORT_API = com.maddin.transportapi.impl.ExampleAPI() // <CUSTOMIZE>
+val WIDGET_CLASS = WidgetActivity::class.java
 
 class MainActivity : com.maddin.echtzeyt.EchtzeytActivity() {
     init {
         // <CUSTOMIZE>
         // Set the api that should be used. You can create your own api, see com.maddin.transportapi for more details.
-        setTransportAPI(com.maddin.transportapi.impl.ExampleAPI())
+        setTransportAPI(TRANSPORT_API)
+        widgetClasses.add(WIDGET_CLASS)
     }
 }
