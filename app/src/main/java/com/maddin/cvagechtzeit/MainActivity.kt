d083df70e7e42179a7f961c1004fc7924c0aaed1
Martin Kunze <martin@martinkunze.com>
2024-01-12 02:50:00 +0100

    adapted to echtzeyt changes

--- app/src/main/java/com/maddin/cvagechtzeit/MainActivity.kt
+++ app/src/main/java/com/maddin/cvagechtzeit/MainActivity.kt
@@ -4,7 +4,7 @@ val TRANSPORT_API = com.maddin.transportapi.impl.germany.VMS("Chemnitz")
 val WIDGET_CLASS = WidgetActivity::class.java
 
 class MainActivity : com.maddin.echtzeyt.EchtzeytActivity() {
-    override val transportStationAPI = TRANSPORT_API
+    override val transportSearchStationAPI = TRANSPORT_API
     override val transportRealtimeAPI = TRANSPORT_API
     override val activitySettings = SettingsActivity::class.java
     override val activityMap = MapActivity::class.java
