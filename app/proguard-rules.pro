ccc11fa1150beda0f0b53627eaae536bba54e7a3
Martin Kunze <martin@martinkunze.com>
2024-01-23 00:16:12 +0100

    added support for the custom vehicle type c-bahn, updated some colors, currently testing on api 15 -> migrating to androidx

--- app/proguard-rules.pro
+++ app/proguard-rules.pro
@@ -19,3 +19,14 @@
 # If you keep the line number information, uncomment this to
 # hide the original source file name.
 #-renamesourcefileattribute SourceFile
+
+-keepnames class * extends androidx.startup.Initializer
+# These Proguard rules ensures that ComponentInitializers are are neither shrunk nor obfuscated,
+# and are a part of the primary dex file. This is because they are discovered and instantiated
+# during application startup.
+-keep class * extends androidx.startup.Initializer {
+    # Keep the public no-argument constructor while allowing other methods to be optimized.
+    <init>();
+}
+
+-keep class androidx.collection.ArrayMap
--- app/src/main/java/com/maddin/cvagechtzeit/CVAGEchtzeit.kt
+++ app/src/main/java/com/maddin/cvagechtzeit/CVAGEchtzeit.kt
@@ -2,11 +2,16 @@ package com.maddin.cvagechtzeit
 
 import android.app.Application
 import com.maddin.echtzeyt.ECHTZEYT_CONFIGURATION
+import com.maddin.echtzeyt.EchtzeytApplication
+import com.maddin.echtzeyt.components.DefaultVehicleTypeResolver
+import com.maddin.echtzeyt.randomcode.IconLineDrawable
 import com.maddin.transportapi.impl.germany.VMS
 
-class CVAGEchtzeit : Application() {
-    override fun onCreate() {
-        super.onCreate()
+class CVAGEchtzeit : EchtzeytApplication() {
+    override fun configure() {
         ECHTZEYT_CONFIGURATION.load(VMS("Chemnitz"))
+        (ECHTZEYT_CONFIGURATION.vehicleTypeResolver as? DefaultVehicleTypeResolver)?.let {
+            it.add(VMS.VT_CHEMNITZ_BAHN, drawable=IconLineDrawable(this, R.color.lineBackgroundChemnitzBahn, R.color.lineForegroundChemnitzBahn, R.drawable.ic_chemnitz_bahn).apply { iconSize = 1.1; iconGravityVertical = 0.7; iconPaddingLeft = 0.25 }, numberResolver={_, line -> line.name.trimStart { c -> c.lowercaseChar() == 'c' }})
+        }
     }
 }
