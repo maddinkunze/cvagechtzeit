183d6a22f356025864960a9e181030f4ad4d4035
Martin Kunze <martin@martinkunze.com>
2024-12-18 18:01:31 +0100

    "working" state, trips are displayed correctly, map is not working

--- app/src/main/java/com/maddin/cvagechtzeit/CVAGEchtzeit.kt
+++ app/src/main/java/com/maddin/cvagechtzeit/CVAGEchtzeit.kt
@@ -1,17 +1,29 @@
 package com.maddin.cvagechtzeit
 
-import android.app.Application
 import com.maddin.echtzeyt.ECHTZEYT_CONFIGURATION
 import com.maddin.echtzeyt.EchtzeytApplication
-import com.maddin.echtzeyt.components.DefaultVehicleTypeResolver
+import com.maddin.echtzeyt.components.DefaultMOTTypeResolver
 import com.maddin.echtzeyt.randomcode.IconLineDrawable
+import com.maddin.transportapi.impl.germany.CVAG
+import com.maddin.transportapi.impl.germany.ChemnitzBahn
 import com.maddin.transportapi.impl.germany.VMS
+import com.maddin.transportapi.impl.germany.VMSandCVAG
 
 class CVAGEchtzeit : EchtzeytApplication() {
     override fun configure() {
-        ECHTZEYT_CONFIGURATION.load(VMS("Chemnitz"))
-        (ECHTZEYT_CONFIGURATION.vehicleTypeResolver as? DefaultVehicleTypeResolver)?.let {
-            it.add(VMS.VT_CHEMNITZ_BAHN, drawable=IconLineDrawable(this, R.color.lineBackgroundChemnitzBahn, R.color.lineForegroundChemnitzBahn, R.color.lineForegroundHintDark, R.drawable.ic_chemnitz_bahn).apply { iconSize = 1.1; iconGravityVertical = 0.7; iconPaddingLeft = 0.25 }, numberResolver={ _, line -> line.name?.trimStart { c -> c.lowercaseChar() == 'c' }})
+        //ECHTZEYT_CONFIGURATION.load(VMS("Chemnitz"))
+        //ECHTZEYT_CONFIGURATION.load(CVAG())
+        ECHTZEYT_CONFIGURATION.load(VMSandCVAG())
+    }
+
+    override fun addMOTTypeBadges() {
+        (ECHTZEYT_CONFIGURATION.motTypeResolver as? DefaultMOTTypeResolver)?.let { resolver ->
+            resolver.add({ it is ChemnitzBahn }, drawable=IconLineDrawable(this, R.color.lineBackgroundChemnitzBahn, R.color.lineForegroundChemnitzBahn, R.color.lineForegroundHintDark, R.drawable.ic_chemnitz_bahn).apply { iconSize = 1.1; iconGravityVertical = 0.7; iconPaddingLeft = 0.25 }, numberResolver={ _, line, variant -> (variant?.name?:line.name)?.trimStart { c -> c.lowercaseChar() == 'c' }})
         }
+        super.addMOTTypeBadges()
+    }
+
+    private fun migrateSettings() {
+        
     }
 }
