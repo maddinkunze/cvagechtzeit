e3cd2254724f1858c4217ee3e1464c452cc75c3f
Martin Kunze <martin@martinkunze.com>
2025-02-24 23:19:52 +0100

    updated map markers to react to clicks/selects, deleted some code of old markers, added shadows back into markers, switched from sublayers to proper layers to display models, reimplemented proper scale and alpha calculations, fixed memory leak where infinite markers would be created, fixed race condition while accessing the model instances, changed licenses to gpl, upgraded to vtm-0.23.0

--- app/src/main/java/com/maddin/cvagechtzeit/MapActivity.kt
+++ app/src/main/java/com/maddin/cvagechtzeit/MapActivity.kt
@@ -2,16 +2,21 @@ package com.maddin.cvagechtzeit
 
 import com.maddin.echtzeyt.activities.MapActivity
 import com.maddin.echtzeyt.randomcode.InstanceData
-import com.maddin.echtzeyt.randomcode.ModelSubLayer
+import com.maddin.echtzeyt.randomcode.ModelLayer
 import com.maddin.echtzeyt.randomcode.WavefrontModel
 import org.oscim.core.GeoPoint
+import kotlin.concurrent.thread
 
 
 class MapActivity : MapActivity() {
-    override fun initCustomModels(layer: ModelSubLayer) {
+    override fun initCustomModels(layer: ModelLayer) {
         super.initCustomModels(layer)
-        val model = WavefrontModel(resources.openRawResource(R.raw.lulatsch), resources.openRawResource(R.raw.lulatschmtl))
-        val instance = InstanceData(GeoPoint(50.857770, 12.923902), scale=1.4)
-        layer.createModelInstance(model, instance)
+
+        thread(start=true, isDaemon=true) {
+            val model = WavefrontModel(resources.openRawResource(R.raw.lulatsch), resources.openRawResource(R.raw.lulatschmtl))
+            model.initNonGlThread()
+            val instance = InstanceData(GeoPoint(50.857770, 12.923902), scale=1.4)
+            layer.createModelInstance(model, instance)
+        }
     }
 }
